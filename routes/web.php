e5d9254532c2e2d2ab8d959255c97f11fc3c90f4
紗倉あんこ <anko0431@gmail.com>
2020-02-08 04:59:31 +0900

    ADD: ルーティングを設定

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// lp
+Route::get('/lp', function(){
+    return view('lp');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// accounts
+Route::get('/account/new', 'AccountsController@new')->name('account.new');
+Route::get('/account/edit', 'AccountsController@edit')->name('account.edit');
+
+// step
+Route::get('/step', 'StepsController@index')->name('step.index');
+Route::get('/step/mypage/register', 'StepsController@mypage_register')->name('step.mypage_register');
+Route::get('/step/mypage/challenge', 'StepsController@mypage_challenge')->name('step.mypage_challenge');
+Route::get('/step/new', 'StepsController@new')->name('step.new');
+Route::get('/step/edit', 'StepsController@edit')->name('step.edit');
+Route::get('/step/ditail/1', 'StepsController@show')->name('step.show');
+
+// step_child
+Route::get('/step/new/1', 'StepsController@child_new')->name('step.child_new');
+Route::get('/step/edit/1/1', 'StepsController@child_edit')->name('step.child_edit');
+
+
