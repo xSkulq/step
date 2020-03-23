803a687fcb09a8b161b4de5f696526ee18bf01f5
紗倉あんこ <anko0431@gmail.com>
2020-03-24 00:32:08 +0900

    ADD: 画像プレビュー機能を追加

---
+++ resources/js/components/step/ImgPreview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="p-account_edit__icon">
+      <input type="file" name="pic" accept="image/*" class="p-account_edit__file" v-preview-input="profileImage">
+      <img :src="profileImage" class="p-account_edit__img" v-if="profileImage">
+      <img alt="no_icon" class="p-account_edit__img" :src="'data:image/png;base64,'+prev_img"  v-else-if="prev_img">
+      <img alt="no_icon" class="p-account_edit__img" src="/imges/no_image.png" v-else>
+    </div>
+  </div>
+</template>
+
+<script>
+import previewInput from 'v-preview-input'
+export default {
+  //inheritAttrs: false, 
+  props: {
+    prev_img:{
+      type: String,
+      required: false
+    },
+  },
+  data: function(){
+    return {
+      profileImage: null,
+    }
+  },
+}
+</script>
