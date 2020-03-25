2ad735d45872324677930aaf52d6b8a465ec0df7
紗倉あんこ <anko0431@gmail.com>
2020-03-25 17:43:47 +0900

    FIX: step一覧のレスポンシブを修正

--- resources/views/step/list.blade.php
+++ resources/views/step/list.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="p-step_mypage">
+<div class="p-step_list">
   <div>
     <!-- main -->
-    <h1 class="p-step_mypage__title">STEP一覧</h1>
+    <h1 class="p-step_list__title">STEP一覧</h1>
 
     <form method="GET" action="{{ route('step.index')}}">
-      <div class="u-flex u-mb80">
+      <div class="p-step_list__search">
         <div class="u-flex">
           <div class="c-search__box">
           <input type="text" class="c-search__input" placeholder="STEP名・達成時間・ユーザー名" name="search" value="@if(!empty('$search')){{ $search }}@endif">
           </div>
-          <button class="p-step_mypage__button">
+          <button class="p-step_list__button">
             <i class="fas fa-search"></i>
           </button>
         </div>
-        <div class="p-step_mypage__select">
+        <div class="p-step_list__select">
           {{ Form::select('category_id', $categories, $category, ['class' => 'c-select', 'id' => 'category_id']) }}
         </div>
       </div>
